f7c0929fb44e6087fe8130694a47bc1d30a83d23
sepehrvafaei <54388517+sepehrvafaei@users.noreply.github.com>
2020-01-13 17:02:21 -0800

    Add files via upload

---
+++ matrixProblem_2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+const int M = 5;
+const int N = 5;
+
+void printMatrixInSpiralOrder(int mat[M][N])
+{
+	int top = 0, bottom = M - 1;
+	int left = 0, right = N - 1;
+
+	while (1)
+	{
+		if (left > right)
+			break;
+		for (int i = left; i <= right; i++)
+			cout << mat[top][i] << " ";
+		top++;
+
+		if (top > bottom)
+			break;
+		for (int i = top; i <= bottom; i++)
+			cout << mat[i][right] << " ";
+		right--;
+
+		if (left > right)
+			break;
+		for (int i = right; i >= left; i--)
+			cout << mat[bottom][i] << " ";
+		bottom--;
+
+		if (top > bottom)
+			break;
+		for (int i = bottom; i >= top; i--)
+			cout << mat[i][left] << " ";
+		left++;
+	}
+}
+
+int main()
+{
+	int mat[M][N] =
+	{
+		{ 1,  2,  3,  4, 5},
+		{16, 17, 18, 19, 6},
+		{15, 24, 25, 20, 7},
+		{14, 23, 22, 21, 8},
+		{13, 12, 11, 10, 9}
+	};
+
+	printMatrixInSpiralOrder(mat);
+
+	return 0;
+}
